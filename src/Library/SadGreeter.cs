b9f17a552343d0934cc52bc30f1d0357b99e0c33
brunoarnuti <brunoarnuti@gmail.com>
2020-09-25 20:57:45 -0300

    realizo el ejercicio

---
+++ src/Library/SadGreeter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Library
+{
+    // Clase de prueba
+    public class SadGreeter
+    {
+        public void SayHiToTheCruelWorld()
+        {
+            Console.WriteLine("Hello Cruel World :(");
+        }
+    }
+}
